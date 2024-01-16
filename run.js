6a2a676c46f46259ccee67961f7edd46b61a39f8
Bluscream <Bluscream@users.noreply.github.com>
2024-01-16 02:08:38 +0100

    Add eval mode option to run.js

--- run.js
+++ run.js
@@ -6,6 +6,7 @@ commander
 .option('-p, --port <value>', 'port number to use, default 3000', 3000)
 .option('-t, --thisip <value>','IP of the interface to bind')
 .option('-b, --broadcastip <value>','IP of the interface to bind','255.255.255.255')
+.option('-e, --eval', 'eval mode, WARNING ⚠️ DO NOT USE THIS IN PRODUCTION')
 .parse(process.argv);
 
 const options = commander.opts()
@@ -61,42 +62,54 @@ var url = require('url')
 var path = require('path')
 const querystring = require('querystring')
 const server = http.createServer((req, res) => {
-  if (req.url === '/favicon.ico') return
-  console.log('[' + req.socket.remoteAddress + '] ' + req.method + ': ' + req.url)
-  const purl = url.parse(req.url); // console.log(purl)
-  const ppath = path.parse(purl.pathname); // console.log(ppath)
-  const query  = querystring.parse(purl.query); //  console.log(query)
-  if (req.url === '/') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.end(
-      '<!DOCTYPE html>\r\n<http><head></head><body><img src="/v.mjpg"></body></html>'
-    )
-  } else if (req.url === '/v.mjpg') {
-    res.setHeader(
-      'Content-Type',
-      'multipart/x-mixed-replace; boundary="xxxxxxkkdkdkdkdkdk__BOUNDARY"'
-    )
-    videoStream.pipe(res)
-  } else if (req.url === '/reconnect') {
-    setupPPPP()
-  } else if (purl.pathname.startsWith('/func/')) { // WARNING ⚠️ DO NOT USE THIS IN PRODUCTION
-    let name = ppath.base
-    let args = ""
-    for (let e in query) {
-      if (args.length > 0) {
-        args += ','
+  try {
+    if (req.url === '/favicon.ico') return
+    console.log('[' + req.socket.remoteAddress + '] ' + req.method + ': ' + req.url)
+    const purl = url.parse(req.url); // console.log(purl)
+    const ppath = path.parse(purl.pathname); // console.log(ppath)
+    const query  = querystring.parse(purl.query); //  console.log(query)
+    if (req.url === '/') {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html; charset=utf-8')
+      res.end(
+        '<!DOCTYPE html>\r\n<http><head></head><body><img src="/v.mjpg"></body></html>'
+      )
+    } else if (req.url === '/v.mjpg') {
+      res.setHeader(
+        'Content-Type',
+        'multipart/x-mixed-replace; boundary="xxxxxxkkdkdkdkdkdk__BOUNDARY"'
+      )
+      videoStream.pipe(res)
+    } else if (req.url === '/reconnect') {
+      setupPPPP()
+    } else if (purl.pathname.startsWith('/func/')) { // WARNING ⚠️ DO NOT USE THIS IN PRODUCTION
+      if (!options.eval) {
+        res.statusCode = 403
+        res.end(JSON.stringify({message: 'eval mode is disabled 🙄'}))
+        return
       }
-      args += e + "=" + query[e]
+      let name = ppath.base
+      let args = ""
+      for (let e in query) {
+        if (args.length > 0) {
+          args += ','
+        }
+        args += e + "=" + query[e]
+      }
+      let eval_str = `p.${name}(${args})`
+      console.log(eval_str)
+      let ret = eval(eval_str)
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify({message: 'ok', result: ret}))
+    } else {
+      res.statusCode = 404
+      res.end(JSON.stringify({message: 'not found'}))
     }
-    let eval_str = `p.${name}(${args})`
-    let ret = eval(eval_str)
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'application/json; charset=utf-8')
-    res.end(JSON.stringify(ret))
-  } else {
-    res.statusCode = 404
-    res.end()
+  } catch (e) {
+    console.log(e)
+    res.statusCode = 500
+    res.end(JSON.stringify({message: e.message, error: e}))
   }
 })
 
